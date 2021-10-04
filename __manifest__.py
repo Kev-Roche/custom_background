5251136ab6da9958877b09d62d8963bfc2e87776
Kevin.roche <kevin.roche@akretion.com>
2021-10-04 14:46:05 +0200

    first step

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Akretion (https://www.akretion.com).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Custom Background",
+    "summary": "change background",
+    "version": "14.0.1.0.0",
+    "category": "Theme/Creative",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "web_responsive",
+    ],
+    "data": [
+        "views/assets.xml",
+    ],
+}
---
+++ readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+1. mettre une image dans static/src/img
+2. mettre son nom ligne 8 dans static/src/css/custom_background.scss
+   par exemple : background: url("../img/randonnee-montagne-tohapi.jpg");
